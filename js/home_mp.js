4dcd5dbdc49ad2d84f23815ea5b3ed1c53fb7ab9
Santiago Alvarez <salvarez@esystemsas.com>
2018-09-18 20:15:36 -0500

    creación del home para mapers

---
+++ js/home_mp.js
@@ -0,0 +1,17 @@
+$( "#service" ).click(function() {
+    location.href="service.html";
+  });
+  
+  $( "#history" ).click(function() {
+    location.href="history.html";
+  });
+  
+  $( "#dates" ).click(function() {
+    location.href="dates.html";
+  });
+  
+  $( "#rating" ).click(function() {
+    location.href="rating.html";
+  });
+  
+  
