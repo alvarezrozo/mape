37273249978e4b56ac6b23dd662d34a85a0b9549
alvarezrozo <alvarezrozo@gmail.com>
2018-10-20 16:59:40 -0500

    se inicia validación de login, sin embargo hay problemas con el controller

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+/*$( "#intro" ).click(function() {
+	var username = $("#user").val();
+	var psw = $("#pass").val();
+	$.ajax({
+		method:"GET",
+		dataType: "jsonp",
+		jsonpCallback: 'callback',
+        url: "http://projectmape.azurewebsites.net/api/Users/?username="+username+"&password="+psw
+    }).then(function(data) {
+    	if(data.Answer){
+    		location.href="home.html?id=22";
+    	}else{
+    		alert("Usario o contraseña incorrectos");
+    	}
+    	alert("Usario o contraseña incorrectos");
+	});
+  
+});*/
+
 $( "#intro" ).click(function() {
-  location.href="home.html";
+	var username = $("#user").val();
+	var psw = $("#pass").val();
+	$.ajax({
+	    type: 'GET',
+	    dataType: 'jsonp',
+	    url: 'http://projectmape.azurewebsites.net/api/Users/?username=andresa&password=prueba',
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+	        if (data != null) {
+
+	            alert("HOLAAAAA");                   
+	        }
+	    }
+	});
 });
+
+
