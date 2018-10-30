a11467b9496df21202920e09a655c60d4bb71ae4
alvarezrozo <alvarezrozo@gmail.com>
2018-10-29 21:43:58 -0500

    corrección de navegación

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-/*$( "#intro" ).click(function() {
-	var username = $("#user").val();
-	var psw = $("#pass").val();
-	$.ajax({
-		method:"GET",
-		dataType: "jsonp",
-		jsonpCallback: 'callback',
-        url: "http://projectmape.azurewebsites.net/api/Users/?username="+username+"&password="+psw
-    }).then(function(data) {
-    	if(data.Answer){
-    		location.href="home.html?id=22";
-    	}else{
-    		alert("Usario o contraseña incorrectos");
-    	}
-    	alert("Usario o contraseña incorrectos");
-	});
-  
-});*/
-
 $( "#intro" ).click(function() {
 	var username = $("#user").val();
 	var psw = $("#pass").val();
