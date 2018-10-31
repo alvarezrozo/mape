b665e84ab7ab5b3138d34649ed2b591a187ab3c3
alvarezrozo <alvarezrozo@gmail.com>
2018-10-30 22:04:29 -0500

    corrección del formato de fecha del datepicker

--- js/service_details.js
+++ js/service_details.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	    url: 'http://projectmape.azurewebsites.net/api/Mapers/'+idmaper,
       	success: function (data) {
          	$(".form_container").append("<p class='title_page'>Nombre: <span class='sub-title'>"+data.Fullname+"</span></p><br><p class='title_page'>Tarifa: <span class='sub-title'>"+data.Cost+"</span></p><br><p class='title_page'>Dispone de: <span class='sub-title'>"+data.Items+"</span></p><br><p class='title_page date'>Fecha de la cita: </p><input data-provide='datepicker' id='datepicker'><br><p class='title_page date'>Hora de la cita: </p><select id='format'><option value='08:00'>08:00</option><option value='08:30'>08:30</option><option value='09:00'>09:00</option><option value='09:30'>09:30</option><option value='10:00'>10:00</option><option value='10:30'>10:30</option><option value='11:00'>11:00</option><option value='11:30'>11:30</option></select><br><i class='fas fa-check-circle menu_check'></i>'");
-         	$("#datepicker").datepicker();
+         	$("#datepicker").datepicker({ dateFormat: 'dd/mm/yy' });
          	$(".menu_check").click(function(){
          		location.href = "index.html";
          	});
