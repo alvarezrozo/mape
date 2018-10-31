4d7d27c3d3ccac98ae1364bd3f8316ed563df9b5
alvarezrozo <alvarezrozo@gmail.com>
2018-10-30 19:58:11 -0500

    llenado dinámico de mapers

--- js/mapers.js
+++ js/mapers.js
@@ -1,18 +1,13 @@
-$( "#id_maper_1" ).click(function() {
-    location.href="service_details.html?id=1";
-});
-
 $(document).ready(function() {
 	var iduser = $.getURLParam("id");
 	$.ajax({
 	    type: 'GET',
 	    dataType: 'json',
 	    url: 'http://projectmape.azurewebsites.net/api/Mapers/',
-      success: function (data) {
-			     console.log(data);
-           for(i in data){
-             console.log(data[i].Fullname);
-           }
-	    }
+      	success: function (data) {
+       		for(i in data){
+         		$(".submenu_container").append("<a href='service_details.html?idMaper="+data[i].IDUser+"'><div class='menu_but' id='id_maper_"+data[i].IDUser+"'><div class='name_rat_container'><div class='name'><p class='map_name'>"+data[i].Fullname+"</p></div><div class='rating'><p class='map_name'>Calificación global: "+data[i].Rate+"</p></div></div><div class='money'><p class='map_name'>"+data[i].Cost+"$</p></div></div></a>");
+      		}	
+    	}
 	});
 });
