1988a134b74d9aaef3f3d3ce7cf13771d1fa8085
alvarezrozo <alvarezrozo@gmail.com>
2018-11-06 09:37:59 -0500

    historial de cliente según su ID listo

--- js/dates.js
+++ js/dates.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  $("#menu_back").click(function(){
+    location.href = "home.html?id="+idclient;
+  });
 	var idclient = $.getURLParam("id");
 	$.ajax({
 	    type: 'GET',
@@ -8,9 +11,6 @@ $(document).ready(function() {
 			    if(data.length != 0){
       			for(i in data){
          			$(".submenu_container").append("<div class='menu_but'><div class='name_rat_container'><div class='name'><p class='map_name'>"+data[i].MaperName+"</p></div><div class='dates'><p class='map_date'>"+data[i].DateD+"</p></div></div><div class='money'><p class='map_name'>"+data[i].Cost+" $</p></div></div>");
-        			$("#menu_back").click(function(){
-                location.href = "home.html?id="+idclient;
-              });
             }
       		}else{
       			alert("No hay citas asignadas");
--- js/history.js
+++ js/history.js
@@ -1,4 +1,22 @@
-$("#menu_back").click(function(){
+$(document).ready(function() {
+  $("#menu_back").click(function(){
+    location.href = "home.html?id="+idclient;
+  });
 	var idclient = $.getURLParam("id");
-	location.href = "home.html?id="+idclient;
+	$.ajax({
+	    type: 'GET',
+	    dataType: 'json',
+	    url: 'http://projectmape.azurewebsites.net/api/Dates/?userID='+idclient+'&f=1',
+      	success: function (data) {
+			    if(data.length != 0){
+      			for(i in data){
+         			$(".submenu_container").append("<div class='menu_but'><div class='name_rat_container'><div class='name'><p class='map_name'>"+data[i].MaperName+"</p></div><div class='dates'><p class='map_date'>"+data[i].DateD+"</p></div></div><div class='money'><p class='map_name'>"+data[i].Cost+" $</p></div></div>");
+            }
+      		}else{
+      			alert("No hay citas asignadas");
+      			location.href = "home.html?id="+idclient;
+      		}
+       		
+    	}
+	});
 });
