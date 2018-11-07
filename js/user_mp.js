4af3a1b3aa02dada71c2236224abd9fbd77d24d8
alvarezrozo <alvarezrozo@gmail.com>
2018-11-06 20:48:24 -0500

    actualizacion de datos de maper LISTO

--- js/user_mp.js
+++ js/user_mp.js
@@ -1,8 +1,31 @@
 $(document).ready(function() {
+   var idmaper = $.getURLParam("id");
+
+   $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: 'http://projectmape.azurewebsites.net/api/Mapers/'+idmaper,
+      success: function (data) {
+         $("#userName").val(data.Username);
+         $("#name").val(data.Name);
+         $("#fullName").val(data.Fullname);
+         $("#password").val(data.Password);
+         $("#mail").val(data.Email);
+         $("#address").val(data.Address);
+         $("#phone").val(data.Phone);
+         $("#cost").val(data.Cost);
+         $("#pss").val(data.Password);
+         $("#implements").val(data.Items);
+         $("#city").val(data.City);
+
+      }
+   });
+
 	$(".menu_check").click(function(){
       if($("#userName").val() != "" && $("#name").val() != "" && $("#cost").val() != "" && $("#fullName").val() != "" && $("#password").val() != "" && $("#mail").val() != "" && $("#address").val() != "" && $("#phone").val() != "" && $("#implements").val() != "" && $("#city").val() != ""){
          if($("#password").val() == $("#pss").val()){
             var params = {
+               "IDUser": idmaper,
                "Username": $("#userName").val(),
                "Name": $("#name").val(),
                "Fullname": $("#fullName").val(),
@@ -17,10 +40,10 @@ $(document).ready(function() {
             $.ajax({
                   data: params,
                   url: 'http://projectmape.azurewebsites.net/api/Mapers/',
-                  type: 'post',
+                  type: 'put',
                   success: function (response) {
-                     alert("Usuario creado con éxito");
-                     location.href = "index_mp.html";
+                     alert("Usuario actualizado con éxito");
+                     location.href = "home_mp.html?id="+idmaper;
                  }
             });
          }else{
