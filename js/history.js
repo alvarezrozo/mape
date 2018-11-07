2aa72b3e2e6e088ff241e855f60be429d4872a83
alvarezrozo <alvarezrozo@gmail.com>
2018-11-06 22:51:39 -0500

    actualización de rating maper

--- js/history.js
+++ js/history.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       	success: function (data) {
 			    if(data.length != 0){
       			for(i in data){
-         			$(".submenu_container").append("<div class='menu_but'><div class='name_rat_container'><div class='name'><p class='map_name'>"+data[i].MaperName+"</p></div><div class='dates'><p class='map_date'>"+data[i].DateD+"</p></div></div><div class='money'><p class='map_name'>"+data[i].Cost+" $</p></div></div>");
+         			$(".submenu_container").append("<div class='menu_but' id=mp_"+"><div class='name_rat_container'><div class='name'><p class='map_name'>"+data[i].MaperName+"</p></div><div class='dates'><p class='map_date'>"+data[i].DateD+"</p></div></div><div class='money'><p class='map_name'>"+data[i].Cost+" $</p></div></div>");
             }
       		}else{
       			alert("No hay citas asignadas");
--- js/mapers.js
+++ js/mapers.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
 	    url: 'http://projectmape.azurewebsites.net/api/Mapers/',
       	success: function (data) {
       		
-          console.log(data);
        		for(i in data){
          		$(".submenu_container").append("<a href='service_details.html?idClient="+idclient+"&idMaper="+data[i].IDUser+"'><div class='menu_but' id='id_maper_"+data[i].IDUser+"'><div class='name_rat_container'><div class='name'><p class='map_name'>"+data[i].Name+" "+data[i].Fullname+"</p></div><div class='rating'><p class='map_name'>Calificación global: "+data[i].Rating+"</p></div></div><div class='money'><p class='map_name'>"+data[i].Cost+"$</p></div></div></a>");
       		}	
